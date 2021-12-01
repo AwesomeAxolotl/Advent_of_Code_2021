b88cf88e57a0e78a8b0e2bd5f2b13ae307aed06b
AwesomeAxolotl <martingampe@metastocks.de>
2021-12-01 08:47:30 +0100

    Add files via upload

--- tag1.py
+++ tag1.py
@@ -18,11 +18,8 @@ print(increases)
 with open("tag1.txt") as f:
     lines = [int(line) for line in f.readlines()]
     increases = 0
-    for i, line in enumerate(lines):
-        if i < len(lines) - 3:
-            sum1 = sum(lines[i:i+3])
-            sum2 = sum(lines[i+1:i+4])
-            increases += sum2 > sum1
+    for i in range(0,len(lines)-3):
+        increases += lines[i+3] > lines[i]
 
 print(increases)   
 
